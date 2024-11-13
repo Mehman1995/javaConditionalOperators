d7e20d6fad87943c475d4dff7957fde38c95dcc9
Mehman <mixailguliev@mail.ru>
2024-11-13 19:21:30 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        // Объявляем переменные для входных данных и параметров программы
+        int initialBalance = 100; // Начальный баланс на счету клиента
+        int topUpAmount = 1000; // Сумма пополнения
+
+        // Рассчитываем итоговый баланс и бонус
+        int totalBalance = initialBalance + topUpAmount;
+        int bonus;
+
+        // Условный оператор для расчёта бонуса
+        if (topUpAmount > 1000) {
+            bonus = (topUpAmount / 100) - 1;
+        } else {
+            bonus = 0;
+        }
+
+        // Выводим результат на экран
+        System.out.println("Итоговый баланс: " + (totalBalance + bonus));
+    }
+}
